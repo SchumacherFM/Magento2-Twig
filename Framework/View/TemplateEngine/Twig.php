f67094040e5a39a3b894043ee9a547d6416d67cd
Cyrill Schumacher <cyrill@schumacher.fm>
2015-01-17 20:31:04 +1100

    Add Magento2 specific functions

--- Framework/View/TemplateEngine/Twig.php
+++ Framework/View/TemplateEngine/Twig.php
@@ -1,12 +1,12 @@
 <?php
-//require_once '../../../../../../../twig/twig/lib/Twig/Autoloader.php';
 
 namespace SchumacherFM\Twig\Framework\View\TemplateEngine;
 
-use Magento\Framework\View\TemplateEngineInterface;
-use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\View\TemplateEngine\Php,
+    Magento\Framework\App\Filesystem\DirectoryList,
+    \Magento\Framework\ObjectManagerInterface;
 
-class Twig implements TemplateEngineInterface
+class Twig extends Php
 {
     const TWIG_CACHE_DIR = 'twig';
 
@@ -21,18 +21,43 @@ class Twig implements TemplateEngineInterface
     protected $_directoryList;
 
     /**
-     * @param \Magento\Framework\Filesystem\DirectoryList $directoryList
+     * @param ObjectManagerInterface $helperFactory
+     * @param DirectoryList $directoryList
      */
-    public function __construct(\Magento\Framework\Filesystem\DirectoryList $directoryList)
-    {
+    public function __construct(ObjectManagerInterface $helperFactory, DirectoryList $directoryList) {
+        parent::__construct($helperFactory);
         $this->_directoryList = $directoryList;
+        $this->initTwig();
+    }
+
+    /**
+     * Inits Twig with all Magento2 necessary functions
+     */
+    private function initTwig() {
         \Twig_Autoloader::register();
         // @todo let users configure the loader. Redis! clear twig cache
+
         $loader = new \Twig_Loader_Filesystem($this->_directoryList->getPath(DirectoryList::ROOT));
+
         $this->twig = new \Twig_Environment($loader, [
             // make it configurable http://twig.sensiolabs.org/doc/api.html#environment-options
             'cache' => $this->_directoryList->getPath(DirectoryList::VAR_DIR) . DIRECTORY_SEPARATOR . self::TWIG_CACHE_DIR,
+            'debug' => true,
         ]);
+
+        $this->twig->addFunction(new \Twig_SimpleFunction('helper', [$this, 'helper']));
+        $this->twig->addFunction(new \Twig_SimpleFunction('block', [$this, '__call']));
+        $this->twig->addFunction(new \Twig_SimpleFunction('get*', [$this, 'catchGet']));
+        $this->twig->addFunction(new \Twig_SimpleFunction('isset', [$this, '__isset']));
+    }
+
+    /**
+     * @return mixed
+     */
+    public function catchGet() {
+        $args = func_get_args();
+        $name = array_shift($args);
+        return $this->__call('get' . $name, $args);
     }
 
     /**
@@ -42,32 +67,40 @@ class Twig implements TemplateEngineInterface
      * the data provided in $vars.
      *
      * @param \Magento\Framework\View\Element\BlockInterface $block
-     * @param string $templateFile
+     * @param string $fileName
      * @param array $dictionary
      * @return string rendered template
      */
     public function render(
         \Magento\Framework\View\Element\BlockInterface $block,
-        $templateFile,
+        $fileName,
         array $dictionary = []
-    )
-    {
-        // $this->twig->addFilter();
-        // use reflection :-( to figure out all getters ...
-        // $filter = new Twig_SimpleFilter('rot13', array('SomeClass', 'rot13Filter'));
-        // remove block specific filters after rendering
-        return $this->getTemplate($templateFile)->render([
-            'data' => $block->getData(), // temp added
-        ]);
+    ) {
+        $this->_currentBlock = $block;
+        return $this->getTemplate($fileName)->render($dictionary);
     }
 
     /**
-     * @param $templateFile
+     * @param $fileName
      * @return \Twig_TemplateInterface
      */
-    private function getTemplate($templateFile)
-    {
-        $tf = str_replace($this->_directoryList->getPath(DirectoryList::ROOT) . DIRECTORY_SEPARATOR, '', $templateFile);
+    private function getTemplate($fileName) {
+        $tf = str_replace($this->_directoryList->getPath(DirectoryList::ROOT) . DIRECTORY_SEPARATOR, '', $fileName);
         return $this->twig->loadTemplate($tf);
+    }
+
+    /**
+     * Get helper singleton
+     *
+     * @param string $className
+     * @return \Magento\Framework\App\Helper\AbstractHelper
+     * @throws \LogicException
+     */
+    public function helper($className) {
+        $helper = $this->_helperFactory->get($className);
+        if (false === $helper instanceof \Magento\Framework\App\Helper\AbstractHelper) {
+            throw new \LogicException($className . ' doesn\'t extends Magento\Framework\App\Helper\AbstractHelper');
+        }
+        return $helper;
     }
 }
