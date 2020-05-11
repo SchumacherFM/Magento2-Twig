cc3fcc5582daae9aad598d4872b863183e574f9b
Thomas Hampe <thampe@users.noreply.github.com>
2020-05-11 12:49:59 +0200

    Add support for include with namespace syntax

--- Twig/Loader/FilesystemLoader.php
+++ Twig/Loader/FilesystemLoader.php
@@ -4,11 +4,22 @@ namespace SchumacherFM\Twig\Twig\Loader;
 
 class FilesystemLoader extends \Twig\Loader\FilesystemLoader
 {
+    
     public function __construct($paths = [], \Magento\Framework\Filesystem\DirectoryList $directoryList)
     {
         $paths[] = './';
         parent::__construct($paths, $directoryList->getRoot());
     }
 
-
+    protected function findTemplate($name, $throw = true)
+    {
+        if(stristr($name, "::") !== false) {
+            $t = $this->resolver->getTemplateFileName($name);
+            $t = str_replace($this->directoryList->getPath(DirectoryList::ROOT) . DIRECTORY_SEPARATOR, '', $t);
+            return parent::findTemplate($t, $throw);
+        } else {
+            return parent::findTemplate($name, $throw);
+        }
+    }
+    
 }
